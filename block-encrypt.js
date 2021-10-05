2c424021854d57dd9430cb1eaa2f15affd8523ae
Gabriel Warkentin <54913241+gwarkentin@users.noreply.github.com>
2021-10-04 17:23:05 -0700

    Update block-encrypt.js
    Added comp8 function

--- block-encrypt.js
+++ block-encrypt.js
@@ -2,6 +2,7 @@
 // Time-stamp: <2021-10-03 07:14:42>
 
 let foo = ["cooper", "is", "awesome"];
+let debug_on = True;
 
 function setup() // P5 Setup Fcn
 {
@@ -42,6 +43,46 @@ function pad_text(vec, charLimit)
 	}
 	return cpy;
 }
+
+//verifies if password passes comp8 requirements, Boolean return
+function comp8(str) {
+	const len_req = 8;
+	if (str.length != len_req) { 
+		if (debug_on) {
+			console.log("Password len " + str.length = " != " + len_req);
+		}
+		return False;
+	}
+	
+	let has_upper = False, has_lower = False, has_symbol = False, has_number = False;
+	
+	for (let i = 0; i < str.length; i++) {
+		let c = str.charCodeAt(i);
+		
+		if (c >= 48 && c <= 57) {
+			has_number = True;
+		}
+		else if (c >= 65 && c <= 90) {
+			has_upper = True;
+		}
+		else if (c >= 97 && c <= 122) {
+			has_lower = True;
+		}
+		// the rest in this range are symbols
+		else if (c >= 32 && c <= 127) {
+			has_symbol = True;
+		}
+	}
+	
+	if (has_upper && has_lower && has_symbol && has_number ) {
+		if (debug_on) { console.log("Password is valid comprehensive8."); }
+		return True;
+	}
+	if (debug_on) { console.log("Password is not valid comprehensive8."); }
+	return False;
+}
+
+
 
 // P5 Frame Re-draw Fcn, Called for Every Frame.
 function draw()

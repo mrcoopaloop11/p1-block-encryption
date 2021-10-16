fa1ff37425a741d73a5067cdc77b6f87c7e71a70
Gabriel Warkentin <gabrielwarkentin@Wack-Mac-Air.local>
2021-10-16 12:20:23 -0700

    redesigned front end, and ssome small changes to other functions

--- assets/draw-stuff.js
+++ assets/draw-stuff.js
@@ -3,68 +3,32 @@
 // ------------------------------------------------------------
 
 
-
-function userDialog(x, y, s, p) {
-    let titlePad = 100;
-
-    // Setup input-box for input and a callback fcn when button is pressed.
-    plaintext = createInput(); // Create an input box, editable.
-    plaintext.position(x, y); // Put box on page.
-	plaintext.size(s);
-
-    password = createInput(); // Create an input box, editable.
-    password.position(x, y + (2 * p)); // Put box on page.
-	password.size(s);
-
-    encryptBtn = createButton( "Encrypt" ); // Create button to help get input data.
-    encryptBtn.position(x + titlePad + 10, y + (4 * p)); // Put button on page.
-	encryptBtn.size(s/2);	
-
-    let titlePlain = createDiv("Plaintext:");
-    titlePlain.position(x - titlePad, y);
-    titlePlain.style('font-size', '18px');
-
-    let titlePass = createDiv("Password:");
-    titlePass.position(x - titlePad, y + (2 * p));
-    titlePass.style('font-size', '18px');
-}
-
-function showStatus() {
-    background(255);
-    textAlign(LEFT);
-	textSize(24);
-	fill(0);
-
-	// encrypted plaintext
-	let ax = encrypt(plaintext.value(), plaintext.value().length);
-	// encrypted plaintext with password
-	let bx = xor_chars(ax, password.value());
-	// decrypted plaintext
-	let cx = xor_chars(bx, password.value());
-    // back to normal plaintext
-    let dx = decrypt(cx);
-
-	let complete_str = "<b>Plaintext Input: </b>" + plaintext.value() + "<br>"
-					+ "<b>Password Input: </b>" + password.value() + "<br>"
-					+ "<b>Encrypted plaintext: </b>" + ax + "<br>"
-					+ "<b>Encrypt plain + pass: </b>" + bx + "<br>"
-					+ "<b>Decrypted plaintext: </b>" + cx + "<br>"
-                    + "<b>Normal plaintext: </b>" + dx;
-                    
-    if(comp8(password.value()) == false) {
-        complete_str = "Invalid password.";
+// Written by Chuck Siska
+// =====================================================  draw_grid ====
+// Draw a fancy grid with major & minor lines 
+// & major row/col numbers.
+function draw_grid( rminor, rmajor, rstroke ) 
+{
+    stroke( rstroke );
+    let sz = g_canvas.cell_size;
+    let width = g_canvas.wid*sz;
+    let height = g_canvas.hgt*sz
+    for ( var ix = 0; ix < width; ix += rminor )
+    {
+        let big_linep = (ix % rmajor == 0);
+        let line_wgt = 1;
+        if (big_linep) line_wgt = 2;
+        strokeWeight( line_wgt );
+        line( ix, 0, ix, height );
+        strokeWeight( 1 );
     }
-                    
-    if(debug_on) {
-        console.log(complete_str);
+    for ( var iy = 0; iy < height; iy += rminor )
+    {
+        let big_linep = (iy % rmajor == 0);
+        let line_wgt = 1;
+        if (big_linep) line_wgt = 2;
+        strokeWeight( line_wgt );
+        line( 0, iy, width, iy );
+        strokeWeight( 1 );
     }
-
-    showEncrypt.remove();
-    showEncrypt = createP(complete_str);
-    showEncrypt.position(50, 150);
-    showEncrypt.style('font-size', '18px');
-	showEncrypt.style('text-align', 'left');
-
-
 }
-
--- block-encrypt.js
+++ block-encrypt.js
@@ -3,46 +3,190 @@
 
 let debug_on = true;
 
-let plaintext;
-let password;
-let encryptBtn;
+// Make global g_canvas JS 'object': a key-value 'dictionary'.
+const g_canvas = { cell_size:20, wid:40.015, hgt:3.78 }; 
+const g_sz = g_canvas.cell_size;
+const g_width = g_sz * g_canvas.wid; // Our 'canvas' uses cells of given size, not 1x1 pixels.
+const g_height = g_sz * g_canvas.hgt;
 
-let showEncrypt;
+const g_seg_size = 7;
+const g_pt_max = 27;
 
-function setup() // P5 Setup Fcn
-{
-	createCanvas(windowWidth - 20, displayHeight - 200);
+var g_plaintext_div; 
+var g_plaintext_label; 
+var g_plaintext; 
+var g_error_pt; 
+
+var g_password_div; 
+var g_password_label; 
+var g_password;
+var g_password_but;
+var g_error_comp8; 
+
+
+var plaintext = ''; //value of string since last button press
+var password = ''; //same for password
+var padded = ''; 
+var chunked = ''; 
+var encrypted = ''; 
+var decrypted = ''; 
+
+var valid_pt = false;
+var valid_pass = false;
+
+// P5 Setup Fcn
+function setup() {
+
+    // Setup input-box for input and a callback fcn when button is pressed.
+	var g_header = createElement('h3', "Encrypt and Decrype string with password");
+
+	//plaintext container, label, and box`
+	g_plaintext_div = createP();
+    g_plaintext_label = createSpan("Plaintext string: ");
+    g_plaintext = createInput(); // Create an input box, editable.
+    g_plaintext.input(update); 
+
+	//put them in container rather than absolute positioning
+	g_plaintext_label.parent(g_plaintext_div);
+	g_plaintext.parent(g_plaintext_div);
+
+	//error label that dissapears when plaintext input meets reqs.
+    g_error_pt = createP( "Max 27 chars, all in printable ASCII range." );
+	g_error_pt.style('color', 'red');
+	g_error_pt.style('font-size', '14px');
+
+
+	//password container, label, and box`
+	g_password_div = createP();
+    g_password_label = createSpan( " Comp8 Password: ");
+    g_password = createInput();
+    g_password.input(update); 
+
+	g_password_but = createButton("Submit");
+	g_password_but.input(update);
+
+	//put them in container rather than absolute positioning
+	g_password_label.parent(g_password_div);
+	g_password.parent(g_password_div);
+	g_password_but.parent(g_password_div);
+
+
+	//error label that dissapears when password meets reqs.
+    g_error_comp8 = createP( "Must be 8 chars and contain >= 1 uppercase, lowercase, number, and symbol." ); 
+	g_error_comp8.style('color', 'red');
+	g_error_comp8.style('font-size', '14px');
+
+
+	// Make a P5 canvas.
+    createCanvas( g_width, g_height );  
 	background(255);
+	
+	update();
+}
 
-	userDialog(150, 50, 200, 15);
-    showEncrypt = createP("");
-	encryptBtn.mousePressed(showStatus);
+// P5 Frame Re-draw Fcn, Called for Every Frame.
+function draw() {
+
+}
+
+function update() {
+ 	// Get data from Input boxes
+    plaintext = g_plaintext.value();
+	password = g_password.value();
+    if (debug_on) {
+		console.log( "plaintext: " + plaintext);
+		console.log( "password: " + password);
+	}
+
+	// check for valid inputs
+	valid_pt = ((0 < plaintext.length) && (plaintext.length <= g_pt_max) && in_ascii_range(plaintext));
+	valid_pass = comp8(password);
+
+	update_tooltips();
+
+	if (valid_pt && valid_pass) {
+		padded = pad_text(plaintext, g_pt_max) + String.fromCharCode(plaintext.length + 32);
+		chunked = seg_str(padded, g_seg_size);
+		encrypted = encrypt(chunked, password);
+		decrypted = encrypt(encrypted, password);
+	}
+	update_grid();
+}
+
+function encrypt(vec, pass) {
+	let enc_vec = []
+	for (v in vec) {
+		enc_vec.push(xor_chars(vec[v],pass));
+	}
+	return enc_vec;
+}
+
+// reset and fill out grid
+function update_grid() {
+	clear();
+	background(255);
+    draw_grid( 25, 8, 'black');
+	if (valid_pt && valid_pass) {
+		textSize(g_canvas.cell_size);
+		textFont('Courier New');
+		print_chunks(chunked, g_sz - 1);
+		print_chunks(encrypted, 2 * g_sz + 4);
+		print_chunks(decrypted, 3 * g_sz + 9);
+	}
+}
+
+// prints one row of chunked strings into grid
+function print_chunks(vec, v_offset) {
+	let l = 7; // x offset, this put text near center of boxes
+	//print chunk at time, box per letter, first letter in chunk red
+	for (let i = 0; i < vec.length; i++) {
+		fill('red');
+		for (let c = 0; c < vec[i].length; c++) {
+			text(vec[i][c], l, v_offset);
+			if (c == 0 ) {
+				fill('black');
+			}
+			l += 25;
+		}
+	}
+}
+
+// hide or unhide the error message tooltips based on input validity 
+function update_tooltips() {
+	if (valid_pt) {
+		g_error_pt.style('visibility', 'hidden');
+	}
+	else {
+		g_error_pt.style('visibility', 'visible');
+	}
+	if (valid_pass) {
+		g_error_comp8.style('visibility', 'hidden');
+	}
+	else {
+		g_error_comp8.style('visibility', 'visible');
+	}
 }
 
 // break up a long string into smaller segments
 // does not modify str arg
-// i.e. calling seg_str("computers are amazing", 5) --> ["compute", "rs are ", "amazing"]
-function seg_str(str, charLimit)
-{
-	let strSeg = {};
+// i.e. calling seg_str("computers are amazing000000", 5) --> ["1compute", "2rs are ", "3amazing", "4000000"]
+function seg_str(str, seg_size) {
+	let strSeg = [];
+
 	let a = 0; //pointer of starting new segment
 
-	for(let c = 0; a < str.length; c++) {
-		strSeg[c] = str.substr(a, charLimit);
-		a += charLimit; //increment pointer to after limit
+	for(let c = 1; a <= str.length; c++) {
+		strSeg.push("" + c + str.substr(a, seg_size));
+		a += seg_size; //increment pointer to after limit
 	}
-
 	return strSeg;
 }
-
 
 // pads the end of the string to fill charLimit count
 // does not modify vec arg
 // can use on single strings OR objects (arays)
-function pad_text(vec, charLimit)
+function pad_text(vec, charLimit, pad = '0')
 {
-	let pad = '0';
-
 	if(typeof(vec) == "string") {
 		return vec.padEnd(charLimit, pad);
 	}
@@ -104,7 +248,7 @@ function comp8(str) {
 	return false;
 }
 
-// returns a string of text encrypted with password
+// returns a string of text encoded with password
 function xor_chars(text, pass) {
 	let text_enc = "";
 	for(let i = 0; i < text.length; i++) {
@@ -113,7 +257,6 @@ function xor_chars(text, pass) {
 		text_enc += String.fromCharCode((tx ^ px) + 32);
 	}
 	return text_enc;
-	
 }
 
 function merge_seg(vec) {
@@ -122,69 +265,4 @@ function merge_seg(vec) {
 		complete += vec[block];
 	}
 	return complete;
-}
-
-function encrypt(plain, len) {
-	let str = "";
-
-	let msg32 = seg_str(plain, 27);
-	for(let c=1; c < Object.keys(msg32).length; c++) {
-		str += encrypt(msg32[c], len);
-	}
-
-	let padInput = pad_text(msg32[0], 27);
-
-	let segInput = seg_str(padInput, 7);
-
-	let segBlocks = Object.keys(segInput).length;
-	for(block in segInput) {
-		segInput[block] = (int(block) + 1) + segInput[block];
-
-		if((segBlocks - 1) == block) {
-			if(debug_on) console.log("The length of the plaintext is: " + len)
-			segInput[block] += String.fromCharCode(len);
-		}
-	}
-	return merge_seg(segInput) + str;
-}
-
-function decrypt(cipher) {
-	let blocksOf = 8;
-	let msg32 = seg_str(cipher, 32);
-	let str = "";
-	for(let c=1; c < Object.keys(msg32).length; c++) {
-		str += decrypt(msg32[c]);
-	}
-
-	let segInput = seg_str(msg32[0], blocksOf);
-	let length;
-
-	let segBlocks = Object.keys(segInput).length;
-	for(block in segInput) {
-		if(int(segInput[block][0]) != ((block % 4) + 1)) {
-			return "Sequence mismatch at block " + int(block + 1);
-		}
-		
-		segInput[block] = segInput[block].slice(1);
-
-		if((segBlocks - 1) == block) {
-			length = segInput[block].charCodeAt(blocksOf - 2);
-			if(debug_on) console.log("Length found from the cipher text: " + length);
-		}
-	}
-
-	str = merge_seg(segInput).slice(0, 27) + str;
-	return str.slice(0, length);
-}
-
-function keyPressed() {
-	if(key == "Enter") {
-		showStatus();
-	}
-}
-
-// P5 Frame Re-draw Fcn, Called for Every Frame.
-function draw()
-{
-
 }
